d9773a4100121443ee4b1c9c3332655ac559ce38
Ethan Fridley <efridley@my.wctc.edu>
2023-02-13 12:55:36 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Module4Assignment;
+using NLog;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // See https://aka.ms/new-console-template for more information
+        string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+        var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+
+        
+
+    }
+}
